df7f62f16ac527bfefaec0ba3075c7d1c6ad692d
Chris Phu <chrisnguyenphu@gmail.com>
2024-05-16 23:50:12 -0500

    Lots of iterating on new styles. Settling on one for now with a button grid on the home page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 
 module.exports = {
     content: ["./**/*.{razor,html,cshtml}"],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                'sliding-background': {
+                    '0%': { backgroundPosition: '0 0' },
+                    '100%': { backgroundPosition: '-4rem 4rem' },
+                },
+            },
+            animation: {
+                'sliding-background': 'sliding-background 10s linear infinite',
+            },
+        },
     },
-    plugins: [],
+    safelist: [
+        {
+            // https://tailwindcss.com/docs/content-configuration#safelisting-classes
+            pattern: /(bg|text)-(red|green|blue|yellow|slate|indigo)-(100|200|300|400|500|600|700|800|900)/,
+            variants: ['hover', 'active'],
+        },
+    ],
 }
