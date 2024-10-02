a327b2e95bcb2a0477120014acba89c5fb9cbf53
Chris Phu <chrisnguyenphu@gmail.com>
2024-10-01 21:50:13 -0500

    Cleaned up last bits of website. Anything incomplete at this time has been marked as WIP. Added attributions on about me page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,13 @@ module.exports = {
   safelist: [
     {
       pattern: /(bg)-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(200)/,
-    },{
+    },
+    {
       pattern: /(text)-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(500)/,
     },
+    {
+      pattern: /(my)-(0|0.5|1|1.5|2|2.5|3|3.5|4|5|6|7|8|9|10)/,
+    }
   ],
   theme: {
     extend: {
